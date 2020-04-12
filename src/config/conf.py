557ce4f4f0a056d384740379658fb20910810a68
Wizard <59536626+WizardSleeps@users.noreply.github.com>
2020-04-12 12:41:47 +0200

    Create conf.py

---
+++ src/config/conf.py
@@ -0,0 +1,4 @@
+bolt_api_key = "XXXX" # BoltIoT Cloud API Key
+device_id = "XXXX" # Device ID for BoltIoT Module
+telegram_chat_id = "@XXXX" # Channel ID for Telegram
+telegram_bot_id = "botXXXX" # Bot ID
